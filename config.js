502b7ee2762d65591de50599f6720b90e567c958
pehesaras570-maker <pehesaras570@gmail.com>
2025-10-07 19:53:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Manu-MD&XBs0ADqR#w5Sfo05KevZGA0TE3fvGMkaap6JwUqYkmCPwU9WHlEg', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&vQl1xbZC#M5C74lGIkTUUiHmpGa1Y5MR25WJTs-E86QLBTPiKh20', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
 MONGODB: process.env.MONGODB || 'Sadee-md@cluster0.vhhguhc.mongodb.net' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
